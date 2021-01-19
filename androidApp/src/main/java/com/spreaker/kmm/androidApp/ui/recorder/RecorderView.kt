2881faf95c09ed044649106037eac46b2d6797da
Alessandro Calzavara <alessandro.calzavara@gmail.com>
2021-01-19 09:42:29 +0100

    use localized strings

--- androidApp/src/main/java/com/spreaker/kmm/androidApp/ui/recorder/RecorderView.kt
+++ androidApp/src/main/java/com/spreaker/kmm/androidApp/ui/recorder/RecorderView.kt
@@ -8,11 +8,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.spreaker.kmm.androidApp.R
 
 @Composable
 fun RecorderView(
@@ -27,13 +29,13 @@ fun RecorderView(
 
     Column(
         modifier = Modifier
-            .padding(padding)
-            .fillMaxWidth()
-            .fillMaxHeight(),
+                .padding(padding)
+                .fillMaxWidth()
+                .fillMaxHeight(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Permissions:", style = TextStyle(fontSize = 25.sp, fontWeight = FontWeight.Bold))
+        Text(text = stringResource(id = R.string.permissions_title), style = TextStyle(fontSize = 25.sp, fontWeight = FontWeight.Bold))
         Text(text = permissionsState)
         Button(onClick = onRequestPermissionsClick) {
             Text("Ask permissions")
@@ -73,7 +75,7 @@ fun RecorderView(model: RecorderViewModel) {
     )
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun PreviewRecorderView() {
     RecorderView(
